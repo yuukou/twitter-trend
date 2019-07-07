3cc47122a53fe452b085317cd4bc9a94bcc2fa37
tanaka <tanaka@logical-studio.com>
2019-07-07 11:13:01 +0900

    index.phpの作製

---
+++ index.php
@@ -0,0 +1,8 @@
+<!--?php require_once __DIR__ . '/functions.php'; ?-->
+<h1>Twitter Trend Ranking</h1>
+<div class="tbl"><!-- 東京（1118370）のトレンドを取得 -->
+    <!--?php echo makeTrendsHtml(getTrends(1118370)); ?-->
+    <!-- 大阪（15015370）のトレンドを取得 -->
+    <!--?php echo makeTrendsHtml(getTrends(15015370)); ?-->
+    <!-- 名古屋（1117817）のトレンドを取得 -->
+    <!--?php echo makeTrendsHtml(getTrends(1117817)); ?--></div>
